0f7708561e194ba9741c7c2564c3a1dd6197911d
MT.Khademi <MT.Khademi@MPC10002236>
2024-12-08 13:29:37 +0330

    refactor: clean file

--- Application/Commands/CommandHandler/LogoutCommandHandler.cs
+++ Application/Commands/CommandHandler/LogoutCommandHandler.cs
@@ -2,27 +2,21 @@
 using Domain.Entities;
 using MediatR;
 using Microsoft.AspNetCore.Identity;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Application.Commands.CommandHandler
+namespace Application.Commands.CommandHandler;
+
+internal class LogoutCommandHandler : IRequestHandler<LogoutCommand, bool>
 {
-    internal class LogoutCommandHandler : IRequestHandler<LogoutCommand, bool>
+    private readonly SignInManager<ApplicationUser> _singInManager;
+
+    public LogoutCommandHandler(SignInManager<ApplicationUser> singInManager)
     {
-        private readonly SignInManager<ApplicationUser> _singInManager;
+        _singInManager = singInManager;
+    }
 
-        public LogoutCommandHandler(SignInManager<ApplicationUser> singInManager)
-        {
-            _singInManager = singInManager;
-        }
-
-        public async Task<bool> Handle(LogoutCommand request, CancellationToken cancellationToken)
-        {
-            await _singInManager.SignOutAsync();
-            return true; 
-        }
+    public async Task<bool> Handle(LogoutCommand request, CancellationToken cancellationToken)
+    {
+        await _singInManager.SignOutAsync();
+        return true; 
     }
 }
