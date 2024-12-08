9fd7b408fb76445596e322c695d33b540c52dd3d
MT.Khademi <MT.Khademi@MPC10002236>
2024-12-08 13:29:00 +0330

    feat: write login and logout command and handlers

---
+++ Application/Commands/CommandHandler/LoginUserCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using Application.Commands.Command;
+using Domain.Entities;
+using Domain.UseCases;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+namespace Application.Commands.CommandHandler;
+
+public class LoginUserCommandHandler : IRequestHandler<LoginUserCommand, bool>
+{
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly SignInManager<ApplicationUser> _signInManager;
+
+    public LoginUserCommandHandler(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+    {
+        _userManager = userManager;
+        _signInManager = signInManager;
+    }
+
+    public async Task<bool> Handle(LoginUserCommand request, CancellationToken cancellationToken)
+    {
+        var user = await _userManager.FindByEmailAsync(request.Email);
+        if (user == null || !(user.IsAuthorized ?? false))
+        {
+            return false;
+        }
+
+        var result = await _signInManager.PasswordSignInAsync(user, request.Password, request.RememberMe,
+            lockoutOnFailure: false);
+        return result.Succeeded;
+
+    }
+}
---
+++ Application/Commands/CommandHandler/LogoutCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using Application.Commands.Command;
+using Domain.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Commands.CommandHandler
+{
+    internal class LogoutCommandHandler : IRequestHandler<LogoutCommand, bool>
+    {
+        private readonly SignInManager<ApplicationUser> _singInManager;
+
+        public LogoutCommandHandler(SignInManager<ApplicationUser> singInManager)
+        {
+            _singInManager = singInManager;
+        }
+
+        public async Task<bool> Handle(LogoutCommand request, CancellationToken cancellationToken)
+        {
+            await _singInManager.SignOutAsync();
+            return true; 
+        }
+    }
+}
