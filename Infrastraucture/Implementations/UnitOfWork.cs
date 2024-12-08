6b310bc166b8d4bb12515b8c92923c2a42cbd12b
MT.Khademi <MT.Khademi@MPC10002236>
2024-12-08 12:05:21 +0330

    feat: write unitOfWork implementation

---
+++ Infrastraucture/Implementations/UnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using Domain.UseCases;
+using Infrastraucture.Data;
+
+namespace Infrastraucture.Implementations;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly VotingContext _context;
+
+    public UnitOfWork(VotingContext context)
+    {
+        _context = context;
+        Users = new UserRepository(_context);
+        Votes = new VoteRepository(_context);
+        VoteGroups = new VoteGroupRepository(_context);
+    }
+
+    public IUserRepository Users { get; private set; }
+
+    public IVoteRepository Votes { get; private set; }
+
+    public IVoteGroupRepository VoteGroups { get; private set; }
+
+    public async Task<int> CompleteAsync() => await _context.SaveChangesAsync();
+
+    public void Dispose() => _context.Dispose();
+}
