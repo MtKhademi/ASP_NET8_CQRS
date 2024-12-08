432d4fb2373febaaf61c37d7a73aaabc8fdbc393
MT.Khademi <MT.Khademi@MPC10002236>
2024-12-08 15:15:05 +0330

    add UpdateVoterCommand

---
+++ Application/Commands/Command/UpdateVoterAuthorizationCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Application.Commands.Command;
+
+internal class UpdateVoterAuthorizationCommand : IRequest<bool>
+{
+    public string UserId { get; set; }
+    public bool IsAuthorized { get; set; }
+}
---
+++ Application/Commands/CommandHandler/UpdateVoterAuthorizationCommandHandler.cs
@@ -0,0 +1,12 @@
+﻿using Application.Commands.Command;
+using MediatR;
+
+namespace Application.Commands.CommandHandler;
+
+internal class UpdateVoterAuthorizationCommandHandler : IRequestHandler<UpdateVoterAuthorizationCommand, bool>
+{
+    public Task<bool> Handle(UpdateVoterAuthorizationCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
