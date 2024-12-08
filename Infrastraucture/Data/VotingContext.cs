8ecb8a08aa16b5e675c67afb1fdb66083eeba544
MT.Khademi <MT.Khademi@MPC10002236>
2024-12-08 10:32:36 +0330

    feat: add connection string and set migration init db

---
+++ Infrastraucture/Data/VotingContext.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastraucture.Data;
+
+public class VotingContext : IdentityDbContext<ApplicationUser>
+{
+    public VotingContext(DbContextOptions<VotingContext> options) : base(options)
+    {
+    }
+
+    public DbSet<VoteGroup> VoteGroups { get; set; }
+    public DbSet<Vote> Votes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<Vote>()
+            .HasOne(v => v.VouteGroup)
+            .WithMany(g => g.Votes)
+            .HasForeignKey(v => v.GroupId);
+    }
+}
