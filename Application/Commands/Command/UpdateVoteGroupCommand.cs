923a9d870bb285fb8caa71b5679fda5fca116c4d
MT.Khademi <MT.Khademi@MPC10002236>
2024-12-09 08:48:30 +0330

    feat: add update voteGroup command and handler

---
+++ Application/Commands/Command/UpdateVoteGroupCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace Application.Commands.Command;
+
+internal class UpdateVoteGroupCommand : IRequest<bool>
+{
+    public int GroupId { get; set; }
+    public string GroupName { get; set; }
+    public string GroupDescription { get; set; }
+}
---
+++ Application/Commands/CommandHandler/UpdateVoteGroupCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using Application.Commands.Command;
+using Domain.UseCases;
+using MediatR;
+
+namespace Application.Commands.CommandHandler;
+
+internal class UpdateVoteGroupCommandHandler : IRequestHandler<UpdateVoteGroupCommand, bool>
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public UpdateVoteGroupCommandHandler(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<bool> Handle(UpdateVoteGroupCommand request, CancellationToken cancellationToken)
+    {
+        var group = _unitOfWork.VoteGroupsRepository.GetById(request.GroupId);
+        if (group == null)
+        {
+            return false;
+        }
+
+        group.GroupName = request.GroupName;
+        group.GroupDescription = request.GroupDescription;
+
+        _unitOfWork.VoteGroupsRepository.Update(group);
+        var result = await _unitOfWork.CompleteAsync();
+        return result > 0;
+    }
+}
